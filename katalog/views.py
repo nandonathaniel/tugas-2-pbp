9a46be6feb5a57dba57019745c0a1822a6f7f04c
HP <fernandonathanielsutanto@gmail.com>
2022-09-14 14:16:12 +0700

    Add all of them

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_katalog(request):
+    Data_Catalog = CatalogItem.objects.all()
+    context = {
+        'nama': 'Fernando Nathaniel Sutanto',
+        'npm' : '2106629995',
+        'all_Catalog': Data_Catalog,
+        
+    }
+    return render(request, "katalog.html", context)
