91e36b53f573677a44511aaf76a506508f24324b
HP <fernandonathanielsutanto@gmail.com>
2022-10-13 10:30:46 +0700

    Add tugas 6

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from todolist.views import register, login_user, logout_user, show_todolist, create_task
+from todolist.views import register, login_user, logout_user, show_todolist, create_task, show_todolist_json, show_todolist_ajax, add_task_ajax
 
 app_name = 'todolist'
 
 urlpatterns = [
-    path('', show_todolist, name='show_todolist'),
+    path('', show_todolist_ajax, name='show_todolist_ajax'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('create-task/', create_task, name='create-task'),
+    path('json/', show_todolist_json, name='show_todolist_json'),
+    path('add/',add_task_ajax, name = 'add_task_ajax'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -8,11 +8,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from todolist.models import Task
 # Create your views here.
 
+#Ga dipake dulu krn ada ajax
 @login_required(login_url='/todolist/login/')
 def show_todolist(request):
     all_tasks = Task.objects.filter(user = request.user)
@@ -21,6 +22,18 @@ def show_todolist(request):
         'pemilik': request.user,
     }
     return render(request, "todolist.html", context)
+
+@login_required(login_url='/todolist/login/')
+def show_todolist_ajax(request):
+    context = {
+        'pemilik': request.user,
+    }
+    return render(request, "todolist.html", context)
+
+@login_required(login_url='/todolist/login/')
+def show_todolist_json(request):
+    all_tasks = Task.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", all_tasks), content_type="application/json")
 
 def register(request):
     form = UserCreationForm()
@@ -42,7 +55,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user) # melakukan login terlebih dahulu
-            response = HttpResponseRedirect(reverse("todolist:show_todolist")) # membuat response
+            response = HttpResponseRedirect(reverse("todolist:show_todolist_ajax")) # membuat response
             response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
             return response
         else:
@@ -62,7 +75,22 @@ def create_task(request):
         desc = request.POST.get("description")
         Task.objects.create(user = request.user, date = timezone.now(), title = judul, description = desc)
 
-        return redirect('todolist:show_todolist')
+        return redirect('todolist:show_todolist_ajax')
 
     context = {'pemilik' : request.user}
     return render(request, 'create-task.html', context)
+
+@login_required(login_url='/todolist/login/')
+def add_task_ajax(request):
+    if request.method == 'POST':
+        this_task = Task()
+        this_task.user = request.user
+        this_task.date = timezone.now()
+        this_task.title = request.POST.get('title')
+        this_task.description = request.POST.get('description')
+
+        this_task.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
